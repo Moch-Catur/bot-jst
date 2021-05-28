766c663087280977ee3196fc77849a71aea685ea
Moch-Catur <84943451+Moch-Catur@users.noreply.github.com>
2021-05-28 12:22:43 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1781217246:AAFHYFBOPzB5DGgqgPVXUotACNdQxRWQ_5I'
+const token = '1733199726:AAH8dPwgZj-U5ClT8tU9XkISoQ1l-00g1Zg'
 const bot = new TelegramBot(token, {polling: true});
 
 
@@ -27,6 +27,44 @@ bot.onText(/\/menu/, (msg) => {
         `this is your main menu`
     );   
 });
+
+state = 0;
+bot.onText(/\/predict/, (msg) => { 
+    bot.sendMessage(
+        msg.chat.id,
+        `masukkan nilai i|v contohnya 9|9`
+    );   
+    state = 1;
+});
+
+bot.on('message', (msg) => {
+    if(state ==1){
+        console.log(msg.text);
+        s = msg.text.split("|");
+        i = s[0]
+        v = s[1]
+        model.predict(
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1])
+            ]
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai v yang diprediksi adalah ${jres[0]} volt`
+            );   
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang diprediksi adalah ${jres[1]} watt`
+            );   
+
+        })
+    }else{
+        state = 0
+
+    }
+} )
+
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
